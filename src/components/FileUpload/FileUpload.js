2728716ab417297bd1eadfc186c4efc4de76d9b8
Nishant <nknishant91@gmail.com>
2023-04-15 10:33:49 +0530

    file upload changeg

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -6,11 +6,24 @@ export default function FileUpload(){
     const handleSubmit = async (event)=>{
         event.preventDefault()
         console.log(form)
-        var myForm = new FormData()
-        Object.keys(form).forEach(ele=>{
-                myForm.append(ele,form[ele]) 
-        })
-      const res =   await uploadImage(myForm)
+
+        var bhejoNaAkkash = new FormData()
+
+        // bhejoNaAkkash.append("productName",form.productName)
+        // bhejoNaAkkash.append("image",form.image)
+
+      // var f =  [["productName","akash"],["image",File]]  // Map
+      //   Object.fromEntries(f)
+      console.log(Object.entries(form));
+        for(let [key,value] of Object.entries(form)){
+          bhejoNaAkkash.append(key,value)
+        }
+
+      //   var myForm = new FormData()
+      //   Object.keys(form).forEach(ele=>{
+      //           myForm.append(ele,form[ele]) 
+      //   })
+       const res =   await uploadImage(bhejoNaAkkash)
       console.log(res)
       if(res.status === "success"){
         console.log(res.message)
@@ -24,7 +37,13 @@ export default function FileUpload(){
     }
 
     const handleChange = async (event)=>{
-        setForm({...form,[event.target.name]:event.target.value})
+        
+        if(event.target.name === "image"){
+          setForm({...form,[event.target.name]:event.target.files[0]})
+        }
+        else{
+          setForm({...form,[event.target.name]:event.target.value})
+        }
 
     }
     return(
